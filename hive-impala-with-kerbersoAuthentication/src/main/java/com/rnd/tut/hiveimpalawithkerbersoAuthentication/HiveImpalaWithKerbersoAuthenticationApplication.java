20555be4bac9b8a95c186864b2270e0879bb2479
Dipen Lama <lamadipen@yahoo.com>
2019-07-13 20:52:28 -0500

    adds the project to git

---
+++ hive-impala-with-kerbersoAuthentication/src/main/java/com/rnd/tut/hiveimpalawithkerbersoAuthentication/HiveImpalaWithKerbersoAuthenticationApplication.java
@@ -0,0 +1,12 @@
+package com.rnd.tut.hiveimpalawithkerbersoAuthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiveImpalaWithKerbersoAuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HiveImpalaWithKerbersoAuthenticationApplication.class, args);
+	}
+}
